aca5f77344ed0edd6e48ada3063215a6968bdb1f
Je5667 <113067745+Je5667@users.noreply.github.com>
2022-09-20 16:53:36 +0900

    확인하시오4

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void) {
-	char c;
+int main(void) {
+	float a,b, c;
+	printf("분자를 입력하세요: ");
+	scanf("%i", &a);
+	printf("분모를 입력하세요: ");
+	scanf("%i", &b);
 	
-	printf("enter a character :");
-	scanf("%c", &c);
-	printf("the next character of %c (%i) is %c (%i)", c, c, c+1, c+1 );
-	return ;
+	c = a / b;
+	printf("나누기의 결과는 %f 입니다. ", c);
+	
+	
+	return 0;
 }
